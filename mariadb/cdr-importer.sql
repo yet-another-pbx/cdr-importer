68888a05f857e23dc75a5d8d7dca696f9ebc9f55
Elliot Michael Keavney <103308993+ellwould@users.noreply.github.com>
2026-08-07 19:19:11 +0100

    Update cdr-importer.sql
    Added SQL

--- mariadb/cdr-importer.sql
+++ mariadb/cdr-importer.sql
@@ -20,3 +20,34 @@ CREATE TABLE `cdr_importer`.`yap_cdr_insert_log` (
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_bin
 ENGINE = InnoDB;
+
+----------------------------------------------------------------------------------------------------
+
+-- Add index
+
+ALTER TABLE `yap_cdr_insert_log`
+ADD INDEX `index___yap_cdr_insert_log__voip_carrier_id` (`voip_carrier_id`);
+
+----------------------------------------------------------------------------------------------------
+
+-- Create foreign key constraint
+
+ALTER TABLE `yap_cdr_insert_log`
+ADD CONSTRAINT fk___yap_cdr_insert_log___voip_carrier
+FOREIGN KEY (`voip_carrier_id`)
+REFERENCES `voip_carrier` (`id`)
+ON UPDATE CASCADE;
+
+----------------------------------------------------------------------------------------------------
+
+-- Create View
+
+CREATE VIEW `view___yap_cdr_insert_log_detail` AS
+SELECT 
+  `voip_carrier`.`id` AS 'voip_carrier_id',
+  `voip_carrier`.`name` AS 'voip_carrier_name',
+  `yap_cdr_insert_log`.`cdr_month_year`,
+  `yap_cdr_insert_log`.`date_time_added`
+FROM `voip_carrier`
+INNER JOIN `yap_cdr_insert_log`
+ON `voip_carrier`.`id` = `yap_cdr_insert_log`.`voip_carrier_id`;
