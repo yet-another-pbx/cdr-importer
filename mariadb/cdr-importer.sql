e6523af8d5431c9e06226957a6a1eb133b7c4d7e
Elliot Michael Keavney <103308993+ellwould@users.noreply.github.com>
2026-08-07 23:04:51 +0100

    Update cdr-importer.sql

--- mariadb/cdr-importer.sql
+++ mariadb/cdr-importer.sql
@@ -12,8 +12,8 @@ COLLATE utf8mb4_bin
 ENGINE = InnoDB;
 
 CREATE TABLE `cdr_importer`.`yap_cdr_insert_log` (
-  `id` SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
-  `voip_carrier_id` BIGINT UNSIGNED NOT NULL,
+  `id` BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
+  `voip_carrier_id` SMALLINT UNSIGNED NOT NULL,
   `cdr_month_year` VARCHAR(255) NOT NULL,
   `date_time_added` DATETIME DEFAULT NOW() NOT NULL,
   PRIMARY KEY(`id`)
