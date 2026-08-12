8041ed693d66011aa49bf422c0de67f40fccc0d0
Elliot Michael Keavney <103308993+ellwould@users.noreply.github.com>
2026-08-12 07:46:13 +0100

    Update install-cdr-importer.sh
    Granted CREATE VIEW to cdr-importer MySQL user

--- install-cdr-importer.sh
+++ install-cdr-importer.sh
@@ -199,7 +199,7 @@ mysql -u root -e "FLUSH PRIVILEGES;";
 mysql -u root -D yap -e "SOURCE /root/cdr-importer/mariadb/cdr-importer.sql;";
 
 # Grant privileges for cdr-importer MariaDB user
-mysql -u root -e "GRANT SELECT, INSERT, UPDATE, CREATE, ALTER, REFERENCES, INDEX ON cdr_importer.* TO 'cdr-importer'@'localhost';";
+mysql -u root -e "GRANT SELECT, INSERT, UPDATE, CREATE, ALTER, CREATE VIEW, REFERENCES, INDEX ON cdr_importer.* TO 'cdr-importer'@'localhost';";
 mysql -u root -e "FLUSH PRIVILEGES;";
 
 #----------------------------------------------------------------------
